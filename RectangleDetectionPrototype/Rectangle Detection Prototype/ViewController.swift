60084b39e9d9717c294b34ebd58fdd407bba751e
Charlie Gerard <charlie.a.gerard@gmail.com>
2017-10-03 16:19:07 +1100

    change folder structure and update code

--- RectangleDetectionPrototype/Rectangle Detection Prototype/ViewController.swift
+++ RectangleDetectionPrototype/Rectangle Detection Prototype/ViewController.swift
@@ -19,15 +19,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //        sceneView.delegate = self
-        //        sceneView.showsStatistics = true
-        //
-        //        let scene = SCNScene()
-        //
-        //        sceneView.scene = scene
-        //
-        //        sceneView.autoenablesDefaultLighting = true
-        
+//        sceneView.delegate = self
+//        sceneView.showsStatistics = true
+//        
+//        let scene = SCNScene()
+//        
+//        sceneView.scene = scene
+//        
+//        sceneView.autoenablesDefaultLighting = true
         
     }
     
@@ -35,19 +34,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillAppear(animated)
         
         startLiveVideo()
-        
-        //        startTextDetection()
+
+//        startTextDetection()
         
         startRectanglesDetection()
         
-        //        let configuration = ARWorldTrackingConfiguration()
-        //        configuration.planeDetection = .horizontal
-        //        sceneView.session.run(configuration)
+//        let configuration = ARWorldTrackingConfiguration()
+//        configuration.planeDetection = .horizontal
+//        sceneView.session.run(configuration)
     }
     
     override func viewDidLayoutSubviews() {
         imageView.layer.sublayers?[0].frame = imageView.bounds
-        //        sceneView.layer.sublayers?[0].frame = sceneView.bounds
+//        sceneView.layer.sublayers?[0].frame = sceneView.bounds
     }
     
     override func didReceiveMemoryWarning() {
@@ -55,7 +54,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    
+
     func startLiveVideo() {
         //1
         session.sessionPreset = AVCaptureSession.Preset.photo
@@ -72,9 +71,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //3
         let imageLayer = AVCaptureVideoPreviewLayer(session: session)
         imageLayer.frame = imageView.bounds
-        //        imageLayer.frame = sceneView.bounds
+//        imageLayer.frame = sceneView.bounds
         imageView.layer.addSublayer(imageLayer)
-        //        sceneView.layer.addSublayer(imageLayer)
+//        sceneView.layer.addSublayer(imageLayer)
         
         session.startRunning()
     }
@@ -89,12 +88,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print("no result")
             return
         }
-        
+
         let result = observations.map({$0 as? VNRectangleObservation})
         
         DispatchQueue.main.async {
             self.imageView.layer.sublayers?.removeSubrange(1...)
-            //            self.sceneView.layer.sublayers?.removeSubrange(1...)
+//            self.sceneView.layer.sublayers?.removeSubrange(1...)
             for rectangle in result {
                 
                 guard let rect = rectangle else {
@@ -104,7 +103,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 self.highlightRectangle(box: rect)
             }
         }
-        
+    
     }
     
     func highlightRectangle(box: VNRectangleObservation){
@@ -115,22 +114,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         toRect.origin.y =  (imageView.frame.size.height) - (imageView.frame.size.height * box.boundingBox.origin.y )
         toRect.origin.y  = toRect.origin.y -  toRect.size.height
         toRect.origin.x =  box.boundingBox.origin.x * imageView.frame.size.width
-        
-        //        toRect.size.width = box.boundingBox.size.width * sceneView.frame.size.width
-        //        toRect.size.height = box.boundingBox.size.height * sceneView.frame.size.height
-        //        toRect.origin.y =  (sceneView.frame.size.height) - (sceneView.frame.size.height * box.boundingBox.origin.y )
-        //        toRect.origin.y  = toRect.origin.y -  toRect.size.height
-        //        toRect.origin.x =  box.boundingBox.origin.x * sceneView.frame.size.width
+
+//        toRect.size.width = box.boundingBox.size.width * sceneView.frame.size.width
+//        toRect.size.height = box.boundingBox.size.height * sceneView.frame.size.height
+//        toRect.origin.y =  (sceneView.frame.size.height) - (sceneView.frame.size.height * box.boundingBox.origin.y )
+//        toRect.origin.y  = toRect.origin.y -  toRect.size.height
+//        toRect.origin.x =  box.boundingBox.origin.x * sceneView.frame.size.width
         
         
         let outline = CALayer()
-        //        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+//        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
         outline.frame = toRect
         outline.borderWidth = 5.0
         outline.borderColor = UIColor.red.cgColor
         
         imageView.layer.addSublayer(outline)
-        //        sceneView.layer.addSublayer(outline)
+//        sceneView.layer.addSublayer(outline)
     }
     
     
@@ -140,77 +139,77 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     // ********************
     
     
-    //    func startTextDetection() {
-    //        let textRequest = VNDetectTextRectanglesRequest(completionHandler: self.detectTextHandler)
-    //        textRequest.reportCharacterBoxes = true
-    //        self.requests = [textRequest]
-    //    }
+//    func startTextDetection() {
+//        let textRequest = VNDetectTextRectanglesRequest(completionHandler: self.detectTextHandler)
+//        textRequest.reportCharacterBoxes = true
+//        self.requests = [textRequest]
+//    }
     
     
-    //    func detectTextHandler(request: VNRequest, error: Error?) {
-    //        guard let observations = request.results else {
-    //            print("no result")
-    //            return
-    //        }
-    //
-    //        let result = observations.map({$0 as? VNTextObservation})
-    //
-    //        DispatchQueue.main.async() {
-    //            self.imageView.layer.sublayers?.removeSubrange(1...)
-    //            for region in result {
-    //                guard let rg = region else {
-    //                    continue
-    //                }
-    //
-    //                self.highlightWord(box: rg)
-    //
-    //                if let boxes = region?.characterBoxes {
-    //                    for characterBox in boxes {
-    //                        //                        self.highlightLetters(box: characterBox)
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
+//    func detectTextHandler(request: VNRequest, error: Error?) {
+//        guard let observations = request.results else {
+//            print("no result")
+//            return
+//        }
+//
+//        let result = observations.map({$0 as? VNTextObservation})
+//
+//        DispatchQueue.main.async() {
+//            self.imageView.layer.sublayers?.removeSubrange(1...)
+//            for region in result {
+//                guard let rg = region else {
+//                    continue
+//                }
+//
+//                self.highlightWord(box: rg)
+//
+//                if let boxes = region?.characterBoxes {
+//                    for characterBox in boxes {
+//                        //                        self.highlightLetters(box: characterBox)
+//                    }
+//                }
+//            }
+//        }
+//    }
     
-    //    func highlightWord(box: VNTextObservation) {
-    //        guard let boxes = box.characterBoxes else {
-    //            return
-    //        }
-    //
-    //        var maxX: CGFloat = 9999.0
-    //        var minX: CGFloat = 0.0
-    //        var maxY: CGFloat = 9999.0
-    //        var minY: CGFloat = 0.0
-    //
-    //        for char in boxes {
-    //            if char.bottomLeft.x < maxX {
-    //                maxX = char.bottomLeft.x
-    //            }
-    //            if char.bottomRight.x > minX {
-    //                minX = char.bottomRight.x
-    //            }
-    //            if char.bottomRight.y < maxY {
-    //                maxY = char.bottomRight.y
-    //            }
-    //            if char.topRight.y > minY {
-    //                minY = char.topRight.y
-    //            }
-    //        }
-    //
-    //        let xCord = maxX * imageView.frame.size.width
-    //        let yCord = (1 - minY) * imageView.frame.size.height
-    //        let width = (minX - maxX) * imageView.frame.size.width
-    //        let height = (minY - maxY) * imageView.frame.size.height
-    //
-    //        let outline = CALayer()
-    //        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
-    //        outline.borderWidth = 2.0
-    //        outline.borderColor = UIColor.red.cgColor
-    //
-    //        imageView.layer.addSublayer(outline)
-    //    }
-    
+//    func highlightWord(box: VNTextObservation) {
+//        guard let boxes = box.characterBoxes else {
+//            return
+//        }
+//        
+//        var maxX: CGFloat = 9999.0
+//        var minX: CGFloat = 0.0
+//        var maxY: CGFloat = 9999.0
+//        var minY: CGFloat = 0.0
+//        
+//        for char in boxes {
+//            if char.bottomLeft.x < maxX {
+//                maxX = char.bottomLeft.x
+//            }
+//            if char.bottomRight.x > minX {
+//                minX = char.bottomRight.x
+//            }
+//            if char.bottomRight.y < maxY {
+//                maxY = char.bottomRight.y
+//            }
+//            if char.topRight.y > minY {
+//                minY = char.topRight.y
+//            }
+//        }
+//        
+//        let xCord = maxX * imageView.frame.size.width
+//        let yCord = (1 - minY) * imageView.frame.size.height
+//        let width = (minX - maxX) * imageView.frame.size.width
+//        let height = (minY - maxY) * imageView.frame.size.height
+//        
+//        let outline = CALayer()
+//        outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
+//        outline.borderWidth = 2.0
+//        outline.borderColor = UIColor.red.cgColor
+//        
+//        imageView.layer.addSublayer(outline)
+//    }
+
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
             return
@@ -235,4 +234,3 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     
 }
-
